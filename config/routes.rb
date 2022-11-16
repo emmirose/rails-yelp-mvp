c218f0997283a37d7e8d53096cc99ad67e725e93
Emma ROSSO <emmarosso.pro@gmail.com>
2022-11-16 15:32:17 +0100

    controllers et views

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants do
+    resources :reviews, only: [:new, :create]
+  end
+
+  get "restaurants", to: "restaurants#index"
+  get "restaurants/:id", to: "restaurants#show"
+  get "restaurants/new", to: "restaurants#new"
+  post "restaurants", to: "restaurants#create"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Restaurant.destroy_all
+Restaurant.create({name:"Le Paon d'Or", address: "Tournai", phone_number: "+3345829525", category:"french"})
+Restaurant.create({name:"Matsuki", address: "Lille", phone_number: "+3345845525", category:"japanese"})
+Restaurant.create({name:"Friterie Meunier", address: "Lille", phone_number: "+3345885525", category:"belgian"})
+Restaurant.create({name:"Papa Posto", address: "Lille", phone_number: "+3347429525",category:"italian"})
+Restaurant.create({name:"Saveurs de Pékin", address: "Paris", phone_number: "+33255829525", category:"chinese"})
